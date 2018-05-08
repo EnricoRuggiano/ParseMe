4bc24aa4ae2c3fcad37eef2c0212c31f7355c7a4
fedus <fedus@Workstation.lan>
2018-05-08 12:28:34 +0200

    added the text.hs code for gchi

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # HaskellFancyTextMe
-Some
+
+Just a simple Haskell Programm which counts the number of Char on a String and creates a ranking.
+
+## How it Works
+
+* open 	`ghci`
+* `:l text.hs`
+* `textMe "MY_FANCY_STRING"`
+
+## Do you want the best and the worst?
+just call `top` and `worst`
+
+ex: 
+ * `let result = textMe "MY_FANCY_STRING"`
+ * `top 10 result   -- to get the top 10 `
+ * `worst 10 result -- to get the worst 10 `	
---
+++ text.hs
@@ -0,0 +1,45 @@
+-- Char ranker of a piece of text
+
+-- Sort
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort'[] = []
+quicksort' (x:xs) = 
+    quicksort' [a | a <- xs, a <= x] ++[x]++ 
+    quicksort' [b | b <- xs, b > x]
+
+-- One List of One Char
+split':: (Eq a)=> [a] -> [a]
+split' [] = []
+split' [x] = [x]
+split' (x:xs)
+    | x /= (head xs) = [x]
+    | otherwise = x: (split' xs)
+
+-- Get a List of splitted list
+splitAll':: (Eq a, Ord a) => [a] -> [[a]]
+splitAll' [] = [[]]
+splitAll' (x:xs) = [(split' (x:xs))] ++ (splitAll' other)
+	where other = [a| a<-xs, a>x]
+
+-- Count the occurencies of the char
+count' :: [[Char]] -> [(Int, Char)]
+count' [[]]  = [(0, '*')]
+count' [[x]] = [(length [x], x)] 
+count' (x:xs) = [(length x, head x)]++ (count' xs)
+
+-- Rank 
+rank' :: (Ord a) =>[a] -> [a]
+rank' x = reverse (quicksort' x)
+
+-- Get the Top x Char
+top :: Int -> [a] -> [a]
+top x y = take x y
+
+-- Get the Worst x Char
+worst :: Int -> [a] -> [a]
+worst x y = take x (reverse y)
+
+-- main function
+textMe :: String -> [(Int, Char)]
+textMe x = rank'(init (count' (splitAll' (quicksort' x)))) 
+				 
